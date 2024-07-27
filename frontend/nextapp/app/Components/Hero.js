d2d8a1c20d9ec0ac8d996f480c6df0275d41d6d0
aadarshsharma <adarsharma56763@gmail.com>
2024-07-27 09:19:50 +0530

    added Components/Hero.js

--- frontend/nextapp/app/Components/Hero.js
+++ frontend/nextapp/app/Components/Hero.js
@@ -1,7 +1,25 @@
+import Image from 'next/image';
+
 export default function Hero() {
-    return (
-        <>
-        <div className="bg-slate-50">anisha</div>
-        </>
-    );
+  return (
+    <div className="grid grid-cols-10 items-center p-8 rounded-lg shadow-md bg-slate-100">
+      <div className="col-span-10 md:col-span-7">
+        <p className="text-4xl ms-12 text-gray-900 font-sans tracking-wide font-normal">
+          Find your Perfect STEM Career Path and turn your Passion into your Profession !
+        </p>
+        <button className="ms-12 mt-8 px-6 py-3 bg-gradient-to-r from-blue-500 to-purple-600 text-white text-lg rounded-full shadow-lg hover:from-blue-600 hover:to-purple-700 focus:outline-none">
+          START →
+        </button>
+      </div>
+      <div className="hidden md:block col-span-3 ml-8">
+        <Image
+          src="/images/scolar-girl.jpg"
+          alt="Graduate"
+          width={300}
+          height={300}
+          className="rounded-2xl"
+        />
+      </div>
+    </div>
+  );
 }
