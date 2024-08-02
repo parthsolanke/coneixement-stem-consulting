b51b53be74f490528713050c72cecb69f762fbc0
Anisha Kumari Lal <96767716+Fr0styfeet@users.noreply.github.com>
2024-08-02 15:33:18 +0530

    Style: Changed colour and sizes
    Changed gradient and size of the divisions in features section

--- frontend/nextapp/app/Components/Features.js
+++ frontend/nextapp/app/Components/Features.js
@@ -1,57 +1,54 @@
+import Image from "next/image";
+
 export default function Features() {
   return (
     <>
-      <section className="mt-10 mb-40">
-
-        <span className="text-4xl font-semibold text-gray-800 flex justify-center md:justify-start mx-40">
+      <section className="mt-20 mb-40">
+        <span className="text-5xl  text-gray-800 flex justify-center md:justify-start mx-40">
           What Do We Offer?
         </span>
 
         <div className="flex flex-col md:flex-row justify-center items-center space-y-10 md:space-y-0 mt-20">
-        <div className="flex justify-center items-center w-48 h-36 rounded-xl text-white font-semibold text-md mx-24" 
-          style={{ background: 'linear-gradient(to left, rgba(255, 0, 0, 0.7), rgba(139, 0, 0, 0.7))' }}>
-          <p className="text-center mx-4">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit1.
-          </p>
-        </div>
-
-          <div
-              className="flex justify-center items-center w-48 h-36 rounded-xl text-white font-semibold text-md mx-24"
-              style={{ background: 'linear-gradient(to left, rgba(59, 130, 250, 0.7), rgba(76, 90, 185, 0.7))' }}
-              ><p className="text-center mx-4">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit3.
-              </p>
-              </div>
-        </div>
-
-        <div className="flex flex-col md:flex-row justify-center items-center mt-10 space-y-10 md:space-y-0">
-        
-      <div
-        className="flex justify-center items-center w-48 h-36 rounded-xl text-white font-semibold text-md mx-4 md:mx-28"
-        style={{ background: 'linear-gradient(to left, rgba(255, 165, 0, 0.7), rgba(220, 110, 0, 0.7))' }}
-        >
-      <p className="text-center mx-4">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit3.
-      </p>
-      </div>
-
-
-          <div className="flex justify-center items-center w-52 h-52 bg-cover bg-center hidden md:block">
-            <img
-              className="object-cover h-full w-full rounded-2xl mt-20"
-              src="./Images/laptop-girl.jpg"
-              alt="Description"
-            />
-          </div>
-          <div className="flex justify-center items-center w-48 h-36 rounded-xl text-white font-semibold text-md mx-4 md:mx-28"
-            style={{ background: 'linear-gradient(to left, rgba(128, 0, 128, 0.7), rgba(75, 0, 130, 0.7))' }}>
-            <p className="text-center mx-4">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit4.
+          <div className="flex justify-center items-center w-60 h-48 rounded-2xl text-white font-semibold text-lg mx-20 bg-red-gradient">
+            <p className="text-center mx-2">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit1 Lorem
+              ipsum dolor sit amet.
             </p>
           </div>
 
+          <div className="flex justify-center items-center w-60 h-48 rounded-2xl text-white font-semibold text-lg mx-20 bg-blue-gradient">
+            <p className="text-center mx-2">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit3 Lorem
+              ipsum dolor sit amet.
+            </p>
+          </div>
         </div>
 
+        <div className="flex flex-col md:flex-row justify-center items-center mt-10 space-y-10 md:space-y-0">
+          <div className="flex justify-center items-center w-60 h-48 rounded-2xl text-white font-semibold text-lg mx-4 md:mx-28 bg-orange-gradient">
+            <p className="text-center mx-2">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit3 Lorem
+              ipsum dolor sit amet.
+            </p>
+          </div>
+
+          <div className="flex justify-center items-center w-72 h-80 bg-cover bg-center hidden md:block">
+            <Image
+              className="object-cover rounded-2xl mt-24"
+              src="/Images/laptop-girl.jpg"
+              alt="Description"
+              layout="responsive"
+              width={100}
+              height={100}
+            />
+          </div>
+          <div className="flex justify-center items-center w-60 h-48 rounded-2xl text-white font-semibold text-lg mx-4 md:mx-28 bg-purple-gradient">
+            <p className="text-center mx-2">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit4 Lorem
+              ipsum dolor sit amet.
+            </p>
+          </div>
+        </div>
       </section>
     </>
   );
--- frontend/nextapp/tailwind.config.js
+++ frontend/nextapp/tailwind.config.js
@@ -10,10 +10,10 @@ module.exports = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "red-gradient": "linear-gradient(to right, #FF2E2E, #F6BE95)",
-        "blue-gradient": "linear-gradient(to right, #0084CE, #E0F7FA)",
-        "orange-gradient": "linear-gradient(to right, #FF862E, #F6BE95)",
-        "purple-gradient": "linear-gradient(to right, #5B57DB, #CFF6FF)"
+        "red-gradient": "linear-gradient(to left, rgba(255, 0, 0, 0.6), rgba(200, 0, 0, 0.8))",
+        "blue-gradient": "linear-gradient(to left, rgba(0, 132, 206, 0.4), rgba(0, 100, 180, 0.7))",
+        "orange-gradient": "linear-gradient(to left, rgba(255, 165, 0, 0.6), rgba(250, 110, 0, 0.7))",
+        "purple-gradient": "linear-gradient(to left, rgba(128, 0, 148, 0.5), rgba(85, 0, 130, 0.7))"
       },
     },
   },
