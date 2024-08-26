6fd271a647794337d3939aeb83dbe330d4c61f48
Anisha Kumari Lal <96767716+Fr0styfeet@users.noreply.github.com>
2024-08-26 13:02:46 +0530

    Minor folder changes

--- frontend/nextapp/app/ageSelect/page.js
+++ frontend/nextapp/app/ageSelect/page.js
@@ -1,11 +1,55 @@
+import Button from "../Components/Button";
 import Navbar from "../Components/Navbar";
-import AgeSelectionPage from "../Components/AgeSelectionPage";
 
 export default function Page() {
     return (
         <>
             <Navbar />
-            <AgeSelectionPage />
+            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 pt-40">
+            <h2 className="text-4xl mb-20 font-bold">Select Your Age Group</h2>
+
+            {/* First Div */}
+            <div className="border-2 rounded-lg p-6 w-full max-w-6xl flex flex-col md:flex-row items-center shadow-sm justify-between mb-6">
+                <div className="w-72 h-56 flex-shrink-0 mr-4">
+                    <img
+                        src="/Images/8th_class.jpeg"
+                        alt="Graduate"
+                        className="rounded-2xl w-full h-full object-cover"
+                    />
+                </div>
+                <div className="m-6 flex-1 text-lg">
+                    <p className="font-bold">
+                        Students in Class 8-10 (Age 13 -15)
+                    </p>
+                    <p>
+                        Decide the stream of your choice and plan the next steps
+                        for your dream career.
+                    </p>
+                </div>
+                <Button display="Select" type="normal" extra="m-4 font-semibold" />
+            </div>
+
+            {/* Second Div */}
+            <div className="border-2 rounded-lg p-6 w-full max-w-6xl flex items-center flex-col md:flex-row  justify-between mb-6">
+                <div className="w-72 h-56 flex-shrink-0 mr-4">
+                    <img
+                        src="/Images/12th_class.jpeg"
+                        alt="Graduate"
+                        className="rounded-2xl w-full h-full object-cover"
+                    />
+                </div>
+                <div className="m-6 flex-1 text-lg">
+                    <p className="font-bold">
+                        Students in Class 11-12 (Age 16 -18)
+                    </p>
+                    <p>
+                        Decide the stream of your choice and plan the next steps
+                        for your dream career.
+                    </p>
+                </div>
+                <Button display="Select" type="normal" extra="m-4 font-semibold" />
+            </div>
+        </div>
         </>
     );
 }
