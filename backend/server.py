a77367d3ac582dc79fdcc4e02f2772a049b75b2e
parthsolanke <parthsolanke2002@gmail.com>
2024-09-20 11:23:37 +0530

    fix: allow cross origin requests

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,17 @@
 # server.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from quiz.routes import app as quiz_app
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.mount("/api", quiz_app)
 
